001344c0b00c1c4a8aee262a67a15163cb46885a
Buffett <1buffetts@gmail.com>
2024-03-23 02:31:13 +0800

    This is our front end

---
+++ script.js
@@ -0,0 +1,26 @@
+var An=true
+var Or=false
+function search() {
+    var input = document.getElementById('searchInput').value;
+    // Perform search using the input
+    console.log('Searching for: ' + input);
+    // You'll need to implement actual search functionality as needed.
+  }
+
+  function OR(){
+    Or=true;
+    An=false;
+    updateSearchCondition();
+  }
+
+  function AND(){
+    An=true;
+    Or=false;
+    updateSearchCondition();
+  }
+
+
+  function updateSearchCondition() {
+    var conditionText = An ? "AND" : "OR";
+    document.getElementById('searchCondition').innerText = "Current Search Condition: " + conditionText;
+}
