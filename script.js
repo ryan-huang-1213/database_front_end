b1f101c2ee4ca2dac9b28091df40dbbb4f48f74c
ryanhuang <ryanhuang1213@gmail.com>
2024-04-29 11:16:20 +0800

    add package.json

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var An=true
-var Or=false
+var An = true;
+var Or = false;
 function search() {
-    var input = document.getElementById('searchInput').value;
-    // Perform search using the input
-    console.log('Searching for: ' + input);
-    // You'll need to implement actual search functionality as needed.
-  }
+  var input = document.getElementById("searchInput").value;
+  // Perform search using the input
+  console.log("Searching for: " + input);
+  // You'll need to implement actual search functionality as needed.
+}
 
-  function OR(){
-    Or=true;
-    An=false;
-    updateSearchCondition();
-  }
+function OR() {
+  Or = true;
+  An = false;
+  updateSearchCondition();
+}
 
-  function AND(){
-    An=true;
-    Or=false;
-    updateSearchCondition();
-  }
+function AND() {
+  An = true;
+  Or = false;
+  updateSearchCondition();
+}
 
-
-  function updateSearchCondition() {
-    var conditionText = An ? "AND" : "OR";
-    document.getElementById('searchCondition').innerText = "Current Search Condition: " + conditionText;
+function updateSearchCondition() {
+  var conditionText = An ? "AND" : "OR";
+  document.getElementById("searchCondition").innerText =
+    "Current Search Condition: " + conditionText;
 }
